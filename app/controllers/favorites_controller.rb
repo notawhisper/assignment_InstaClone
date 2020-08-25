e051f280432a4d0d7b06c0ee4bb471f73420403c
shotaro yamamoto <shotoron@gmail.com>
2020-08-25 16:03:00 +0900

    [add]お気に入り登録/解除機能を作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,11 @@
 class FavoritesController < ApplicationController
-  def 
+  def create
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_url, notice: "お気に入りに登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_url, notice: "お気に入りから解除しました"
+  end
 end
