4e83a917b8548713da06fe9efb863eafb05f1ece
shotaro yamamoto <shotoron@gmail.com>
2020-08-26 00:57:20 +0900

    [fix]アクセス制御メソッドとログアウト後の遷移先をトップページに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  def redirect_to_root
+    redirect_to root_path
+  end
+
   def access_controll
     if @picture.present?
       unless current_user.id == @picture.user_id
         flash[:notice] = "本人のみにアクセスが許可されています。"
-        redirect_to pictures_path
+        redirect_to_root
       end
     elsif @user.present?
       unless current_user.id == @user.id
         flash[:notice] = "本人のみにアクセスが許可されています。"
-        redirect_to pictures_path
+        redirect_to_root
       end
     else
     end
@@ -20,7 +24,7 @@ class ApplicationController < ActionController::Base
   def exclude_stranger
     unless logged_in?
       flash[:notice] = "ログインしてください。"
-      redirect_to new_session_path
+      redirect_to_root
     end
   end
 end
