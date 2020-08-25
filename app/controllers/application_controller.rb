1ec381a270b61cb58c0cefd6111daf79f6221d6e
shotaro yamamoto <shotoron@gmail.com>
2020-08-26 00:15:31 +0900

    [add]application_controller.rbにアクセス制御のメソッドを追加、users_controller.rbとpictures_controller.rbで呼び出しを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def access_controll
+    if @picture.present?
+      unless current_user.id == @picture.user_id
+        flash[:notice] = "本人のみにアクセスが許可されています。"
+        redirect_to pictures_path
+      end
+    elsif @user.present?
+      unless current_user.id == @user.id
+        flash[:notice] = "本人のみにアクセスが許可されています。"
+        redirect_to pictures_path
+      end
+    else
+    end
+  end
 end
