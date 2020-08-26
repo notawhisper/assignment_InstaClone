d8bf47dc400b0ec34c0dbb98963aa4e50a7fda48
shotaro yamamoto <shotoron@gmail.com>
2020-08-26 14:39:23 +0900

    [add]letter_opener_webの設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
       get :index
     end
   end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
