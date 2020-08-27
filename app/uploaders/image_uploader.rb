3f2e17cd5ab25d5254316fa0838ccc45c137f5e3
shotaro yamamoto <shotoron@gmail.com>
2020-08-27 16:28:48 +0900

    [fix]ヘッダーのレイアウト作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fill: [30, 30, "center"]
+  end
+
+  version :profile do
+    process resize_to_fill: [200,200, "center"]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
