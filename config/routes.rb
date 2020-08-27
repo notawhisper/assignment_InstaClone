c11af59abb668646fa734912ef31e6c71f16a0a2
shotaro yamamoto <shotoron@gmail.com>
2020-08-27 18:27:51 +0900

    [fix]トップページをsessions#newに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'root#index'
+  root 'sessions#new'
+  get 'sessions', to: 'sessions#new'
   resources :pictures
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
