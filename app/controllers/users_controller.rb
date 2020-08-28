419fa62deaca3da91bc2507c7f335f8870ff439a
shotaro yamamoto <shotoron@gmail.com>
2020-08-28 11:09:23 +0900

    [delete]不要な記述、ファイルを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,28 +3,20 @@ class UsersController < ApplicationController
   before_action :access_controll, only: [:show, :edit, :update, :destroy]
   before_action :exclude_stranger, except: [:new, :create]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
     if @user.save
@@ -36,8 +28,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
      if @user.update(user_params)
        redirect_to @user, notice: "プロフィールを編集しました！"
@@ -45,8 +35,7 @@ class UsersController < ApplicationController
        render :edit
      end
   end
-  # DELETE /users/1
-  # DELETE /users/1.json
+
   def destroy
     @user.destroy
     respond_to do |format|
@@ -56,12 +45,10 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name, :email, :password_digest, :password, :image, :image_cache)
   end
