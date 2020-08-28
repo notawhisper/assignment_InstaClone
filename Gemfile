a8664c144d6c531a59309e067102a061b0fde898
shotaro yamamoto <shotoron@gmail.com>
2020-08-28 20:25:38 +0900

    [fix] letter_opener_webに変更

--- Gemfile
+++ Gemfile
@@ -34,13 +34,9 @@ group :test do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
 gem 'bcrypt', '3.1.11'
-
 gem 'carrierwave'
-
 gem 'mini_magick'
-
 gem 'faker'
 
 # 全体に適用させる
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 # }
 
   config.action_mailer.default_url_options = { host: 'https://rocky-forest-95710.herokuapp.com/' }
-  ActionMailer::Base.delivery_method = :letter_opener
+  ActionMailer::Base.delivery_method = :letter_opener_web
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
